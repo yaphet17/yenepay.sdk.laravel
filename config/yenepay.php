99cc91235c152514e9611d38ec1120abb12e7268
yaphet17 <yafetberhanu3@gmail.com>
2022-08-11 18:35:16 +0300

    Remove boilerplate if statement

--- config/yenepay.php
+++ config/yenepay.php
@@ -3,6 +3,6 @@ return [
     // your yenepay sellercode
     'sellerCode' => env('YP_SELLER_CODE'),
 
-    // your enviroment eg. PROD if you are in production, TEST if you are in testing mode
-    'sandBox' => env('YP_ENV')
+    // your enviroment eg. PROD if you are in production, SANDBOX if you are in testing mode
+    'env' => env('YP_ENV')
 ];
--- src/YenePay.php
+++ src/YenePay.php
@@ -28,7 +28,7 @@ class YenePay
         // get the checkout items as key-value pair added with the checkoutOptions array
         $queryString = $item->getAsKeyValue($optionsDict);
         $queryString = http_build_query($queryString);
-        if (is_null($checkoutOptions->getUseSandbox()) && $checkoutOptions->getUseSandbox() == 'yes')
+        if (isInSandbox())
             return self::CHECKOUTBASEURL_SANDBOX . '?' . $queryString;
         return self::CHECKOUTBASEURL_PROD . '?' . $queryString;
     }
@@ -46,7 +46,7 @@ class YenePay
             }
         }
         $queryString = http_build_query($optionsDict);
-        if (is_null($checkoutOptions->getUseSandbox()) && $checkoutOptions->getUseSandbox() == 'yes')
+        if (isInSandbox())
             return self::CHECKOUTBASEURL_SANDBOX . '?' . $queryString;
         return self::CHECKOUTBASEURL_PROD . '?' . $queryString;
     }
@@ -55,7 +55,7 @@ class YenePay
     {
         //get ipnmodel dictionary
         $ipnDict = $ipnModel->getAsKeyValue();
-        $ipnUrl = (is_null($ipnModel->getUseSandbox()) && $ipnModel->getUseSandbox() == 'yes') ? self::IPNVERIFYURL_SANDBOX : self::IPNVERIFYURL_PROD;
+        $ipnUrl = isInSandbox() ? self::IPNVERIFYURL_SANDBOX : self::IPNVERIFYURL_PROD;
         try {
             $response = Http::acceptJson()->post($ipnUrl, $ipnDict);
             if ($response->status() == 200)
@@ -71,7 +71,7 @@ class YenePay
     {
         //get pdtmodel dictionary
         $pdtDict = $pdtModel->getAsKeyValue();
-        $pdtUrl = (is_null($pdtModel->getUseSandbox()) && $pdtModel->getUseSandbox() == 'yes') ? self::PDTURL_SANDBOX : self::PDTURL_PROD;
+        $pdtUrl = isInSandbox() ? self::PDTURL_SANDBOX : self::PDTURL_PROD;
         try {
             $response = Http::acceptJson()->post($pdtUrl, $pdtDict);
             return $response->throw()->body();
@@ -81,5 +81,13 @@ class YenePay
         }
         $result['result'] = "FAIL";
         return $result;
+    }
+
+    private function isInSandbox(){
+        $env = strtoupper(config('yenepay.env'));
+        if($env == "SANDBOX"){
+            return true;
+        }
+        return false;
     }
 }
